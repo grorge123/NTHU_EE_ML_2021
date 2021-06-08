0c6aa8d7193a777e0f753e89253952d5bf31b035
grorge <g97116@yahoo.com>
2021-06-08 23:02:42 +0800

    add season

--- ML.py
+++ ML.py
@@ -9,30 +9,45 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.tree import DecisionTreeClassifier as dtc
 from  sklearn.ensemble import RandomForestClassifier
 import csv
+from sklearn.metrics import f1_score
 import random
+
+
+myseed = 7122
+random.seed(myseed)
+np.random.seed(myseed)
 
 tr_path = 'deal3_train.csv'  # path to training data
 tt_path = 'deal2_test.csv'   # path to te|sting data
-se_path = 'season.csv'
 
-x = []
-y = []
+x_va = []
+y_va = []
 with open(tr_path, 'r', encoding="Big5") as fp:
             data = list(csv.reader(fp))
             data = np.array([list(map(float,i)) for i in data[1:]]).astype(float)
             random.shuffle(data)
-            x_tr = [data[i][2:] for i in range(len(data)) if i % 10 <= 7]
-            y_tr = [data[i][1] for i in range(len(data)) if i % 10 <= 7]
-            x_va = [data[i][2:] for i in range(len(data)) if i % 10 > 7]
-            y_va = [data[i][1] for i in range(len(data)) if i % 10 > 7]
+            x_tr = [data[i][2:] for i in range(len(data)) if i % 10 <= 8]
+            y_tr = [data[i][1] for i in range(len(data)) if i % 10 <= 8]
+            x_va = [data[i][2:] for i in range(len(data)) if i % 10 > 8]
+            y_va = [data[i][1] for i in range(len(data)) if i % 10 > 8]
+feature_filter = [0,43,37,80,38,81]
+feature = []
+for i in range(len(x_tr[0])):
+    if(i not in feature_filter):
+        feature.append(i)
+x_tr = list(np.array(x_tr)[:, feature])
+x_va = list(np.array(x_va)[:, feature])
+print("TEST:", len(x_tr), len(x_tr[0]))
 model = [LogisticRegression(), GaussianNB(), RandomForestClassifier(), dtc()]
 for i in model:
     mo = i
     mo.fit(x_tr, y_tr)
     print(i, mo.score(x_tr, y_tr))
-    predict = mo.predict(x_va)
+    y_pred = mo.predict(x_tr)
+    print(f1_score(y_tr, y_pred, average='binary'))
+    y_pred = mo.predict(x_va)
     print(i, mo.score(x_va, y_va))
-    print(list(y_va).count(1), list(predict).count(1))
+    print(f1_score(y_va, y_pred, average='binary'))
 pred = RandomForestClassifier()
 pred.fit(x_tr, y_tr)
 with open(tt_path, 'r', encoding="Big5") as fp:
@@ -40,6 +55,7 @@ with open(tt_path, 'r', encoding="Big5") as fp:
             data = np.array([list(map(float,i)) for i in data[1:]]).astype(float)
             x_t = data[:, 2:]
             label = data[:, 0]
+x_t = list(np.array(x_t)[:, feature])
 predict_final = pred.predict(x_t)
 print(list(predict_final).count(1))
 
